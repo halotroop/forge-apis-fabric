8dd3827fc79af673141716386f885b11c40e3a2c
Caroline Bell <carolinejoybell@gmail.com>
2022-12-16 00:13:18 -0800

    Add AlphaMode's ForgeTags
    Co-authored-by: alpha <alphamodeyt@gmail.com>

---
+++ src/main/java/me/alphamode/forgetags/data/FluidTagProvider.java
@@ -0,0 +1,17 @@
+package me.alphamode.forgetags.data;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+
+/**
+ * @author AlphaMode
+ */
+public class FluidTagProvider extends FabricTagProvider.FluidTagProvider {
+    public FluidTagProvider(FabricDataGenerator dataGenerator) {
+        super(dataGenerator);
+    }
+
+    @Override
+    protected void generateTags() {
+    }
+}
